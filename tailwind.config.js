0f960ed997424d807154f2b4d10de4fd02a98164
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-12-28 14:05:50 +0700

    time count update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
-        keyframes: {
-            smokeRise: {
-                '0%': { opacity: '1', transform: 'translateY(0) scale(1)' },
-                '100%': { opacity: '0', transform: 'translateY(-150px) scale(1.5)' },
-            },
-        },
-        animation: {
-            smoke: 'smokeRise 2s ease-in-out forwards',
-        },
-        fontFamily: {
-            sacramento: ['Sacramento', 'cursive'], // Tambahkan font sesuai kebutuhan
-        },
+        // keyframes: {
+        //     smokeRise: {
+        //         '0%': { opacity: '1', transform: 'translateY(0) scale(1)' },
+        //         '100%': { opacity: '0', transform: 'translateY(-150px) scale(1.5)' },
+        //     },
+        // },
+        // animation: {
+        //     smoke: 'smokeRise 2s ease-in-out forwards',
+        // },
+        // fontFamily: {
+        //     sacramento: ['Sacramento', 'cursive'], // Tambahkan font sesuai kebutuhan
+        // },
     },
     },
   plugins: [],
