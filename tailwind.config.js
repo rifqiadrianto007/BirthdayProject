7e2cc3aa491d15d50fe26c16ddd04f6eae437564
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-12-28 20:44:29 +0700

    animation balloon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,17 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
-        // keyframes: {
-        //     smokeRise: {
-        //         '0%': { opacity: '1', transform: 'translateY(0) scale(1)' },
-        //         '100%': { opacity: '0', transform: 'translateY(-150px) scale(1.5)' },
-        //     },
-        // },
-        // animation: {
-        //     smoke: 'smokeRise 2s ease-in-out forwards',
-        // },
-        // fontFamily: {
-        //     sacramento: ['Sacramento', 'cursive'], // Tambahkan font sesuai kebutuhan
-        // },
+      animation: {
+        floating: 'floating 3s ease-in-out infinite',
+      },
+      keyframes: {
+        floating: {
+          '0%, 100%': { transform: 'translateY(-10px)' },
+          '50%': { transform: 'translateY(10px)' },
+        },
+      },
     },
-    },
+  },
   plugins: [],
 };
 
