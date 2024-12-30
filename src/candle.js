a2f37722d8b3ec39cfdd67b366a2071aeaaec271
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-12-30 14:55:34 +0700

    candle done

--- src/candle.js
+++ src/candle.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).addClass("puff-bubble");
         });
         $("#glow").remove();
-        txt.hide().html("i wish you happy birthday").delay(750).fadeIn(300);
+        txt.hide().html("Aamiin... Semoga semua doa dan harapan adek terwujud ya cantikk 😘").delay(750).fadeIn(300);
         $("#candle").animate(
             {
             opacity: ".5"
