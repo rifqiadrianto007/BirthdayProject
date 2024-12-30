484ad326ccbf8c673f50cafb78b123c39bf5ba45
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-12-30 21:51:47 +0700

    candle update

--- src/candle.js
+++ src/candle.js
@@ -1,23 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(function () {
-    var flame = $("#flame");
-    var txt = $("h1");
+        var flame = $("#flame");
+        var txt = $("h1");
+        var redirectButton = $("#redirectButton");
 
-    flame.on({
-        click: function () {
-        flame.removeClass("burn").addClass("puff");
-        $(".smoke").each(function () {
-            $(this).addClass("puff-bubble");
-        });
-        $("#glow").remove();
-        txt.hide().html("Aamiin... Semoga semua doa dan harapan adek terwujud ya cantikk 😘").delay(750).fadeIn(300);
-        $("#candle").animate(
-            {
-            opacity: ".5"
+        flame.on({
+            click: function () {
+                // Matikan api lilin
+                flame.removeClass("burn").addClass("puff");
+                $(".smoke").each(function () {
+                    $(this).addClass("puff-bubble");
+                });
+                $("#glow").remove();
+
+                // Tampilkan pesan "Aamiin..."
+                txt.hide()
+                    .html("Aamiin... Semoga semua doa dan harapan adek terwujud ya cantikk 😘")
+                    .delay(750)
+                    .fadeIn(300);
+
+                $("#candle").animate(
+                    {
+                        opacity: "0.5",
+                    },
+                    100
+                );
+
+                // Setelah 3 detik, gantikan teks dengan "Lilin sudah mati" dan tombol
+                setTimeout(function () {
+                    txt.fadeOut(300, function () {
+                        txt.html("Lilin sudah mati sayangg 😊");
+                        txt.fadeIn(300);
+
+                        // Setelah 2 detik lagi, gantikan dengan tombol
+                        setTimeout(function () {
+                            txt.fadeOut(300, function () {
+                                txt.html(""); // Kosongkan teks
+                                redirectButton.removeClass("hidden").fadeIn(300);
+                            });
+                        }, 2000);
+                    });
+                }, 3000);
             },
-            100
-        );
-        }
+        });
     });
-});});
-
+});
